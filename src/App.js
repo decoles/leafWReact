b2427125ae6d50ab548e8e4c1eeaa3569f9ca451
decoles <davidepcoles@gmail.com>
2023-01-10 02:12:58 -0800

    inital commit with base files and example

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
+
+export default App;
